017f4e1d0a97cdaf106d74b6506b335e10e24db4
chrisle <hung.leviet@powergatesoftware.com>
2022-04-26 09:34:48 +0700

    init

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => (
+  <h1>Hello World</h1>
+)
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("main")
+)
